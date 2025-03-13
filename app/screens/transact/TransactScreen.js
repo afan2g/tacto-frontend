dd1875c84ef73b33f33f473661339242b6f5c958
afan2g <125404099+afan2g@users.noreply.github.com>
2025-03-13 01:05:15 -0400

    added handling for transaction value inputs

--- app/screens/transact/TransactScreen.js
+++ app/screens/transact/TransactScreen.js
@@ -1,5 +1,5 @@
-import React, { useContext, useEffect, useCallback } from "react";
-import { useFocusEffect } from "@react-navigation/native";
+import React, { useContext, useEffect, useState } from "react";
+import RNHapticFeedback from "react-native-haptic-feedback";
 import { View, StyleSheet } from "react-native";
 import { AppButton, AppText, Screen } from "../../components/primitives";
 import AppKeypad from "../../components/forms/AppKeypad";
@@ -7,10 +7,12 @@ import { TransactionContext } from "../../contexts/TransactionContext";
 import { colors, fonts } from "../../config";
 import { useKeypadInput } from "../../hooks/useKeypadInput";
 import routes from "../../navigation/routes";
+import { useData } from "../../contexts";
 
 function TransactScreen({ navigation }) {
   const { transaction, setTransaction, clearTransaction } = useContext(TransactionContext);
-
+  const [error, setError] = useState(null);
+  const { wallet } = useData();
   // Use the custom hook with initial value and options
   const { value, setValue, handleKeyPress, resetValue } = useKeypadInput("", {
     maxDecimalPlaces: 2,
@@ -20,6 +22,7 @@ function TransactScreen({ navigation }) {
 
   // Update transaction.amount whenever value changes
   useEffect(() => {
+    setError(null);
     setTransaction((prev) => ({
       ...prev,
       amount: value,
@@ -34,6 +37,20 @@ function TransactScreen({ navigation }) {
 
 
   const handleSend = () => {
+    if (!value || value === "") {
+      setError("Please enter an amount");
+      RNHapticFeedback.trigger("notificationWarning", {
+        ignoreAndroidSystemSettings: false,
+      })
+      return;
+    } else if (parseFloat(value) > parseFloat(wallet.usdc_balance)) {
+      setError("Insufficient balance");
+      RNHapticFeedback.trigger("notificationWarning", {
+        ignoreAndroidSystemSettings: false,
+      });
+      return;
+    }
+
     setTransaction((prev) => ({
       ...prev,
       action: "Sending",
@@ -42,6 +59,13 @@ function TransactScreen({ navigation }) {
   };
 
   const handleRequest = () => {
+    if (!value || value === "") {
+      setError("Please enter an amount");
+      RNHapticFeedback.trigger("notificationWarning", {
+        ignoreAndroidSystemSettings: false,
+      })
+      return;
+    }
     setTransaction((prev) => ({
       ...prev,
       action: "Requesting",
@@ -56,6 +80,7 @@ function TransactScreen({ navigation }) {
       >
         ${value === "" ? "0" : value}
       </AppText>
+      <AppText style={styles.error}>{error}</AppText>
       <View style={styles.input}>
         <View style={styles.sendReceiveContainer}>
           <AppButton
@@ -114,6 +139,10 @@ const styles = StyleSheet.create({
   buttonText: {
     fontFamily: fonts.bold,
     fontSize: 24,
+  },
+  error: {
+    color: colors.red,
+    fontSize: 16,
   },
 });
 
